429ace59dba359b9808f8a50180dd51f27e38be5
Your Name <you@example.com>
2026-03-17 16:55:38 +0530

    Renamed file properly

--- Day-001/task 1.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the Band Name Generator!")
-name = input("Which city did you grow up in?\n")
-pet_name = input("What is the name of your pet?\n")
-print("Your band name could be: " + name + pet_name)
